bbd136d9310388eb63038d961ac21452c8c00cdb
David Jensenius <david@jensenius.com>
2020-12-13 12:11:40 -0500

    Initial Commit

---
+++ Shared/FluxHausApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluxHausApp.swift
+//  Shared
+//
+//  Created by David Jensenius on 2020-12-13.
+//
+
+import SwiftUI
+
+@main
+struct FluxHausApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
