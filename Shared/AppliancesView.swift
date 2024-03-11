e188f643a041ed20249c032b6ba75857012de2f0
David Jensenius <david@jensenius.com>
2024-03-11 18:22:42 -0400

    Remove battery from macOS because it's not reliable

--- Shared/AppliancesView.swift
+++ Shared/AppliancesView.swift
@@ -39,30 +39,33 @@ struct Appliances: View {
         ScrollView {
                 LazyVGrid(columns: gridItemLayout, spacing: 5) {
                     ForEach((0..<6), id: \.self) { i in
-                        if (getApplianceName(type: theAppliances[i].name, index: theAppliances[i].index) != "Fetching") {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 15, style: .continuous)
-                                    .fill(Color(UIColor.systemGray6))
-                                HStack {
-                                    VStack(alignment: .leading) {
-                                        HStack {
-                                            Text(getIcon(type: theAppliances[i].name, index: theAppliances[i].index))
-                                                .font(.title2)
+                        if !(theAppliances[i].name == "Battery" && battery.model == .mac) {
+                            if (getApplianceName(type: theAppliances[i].name, index: theAppliances[i].index) != "Fetching") {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 15, style: .continuous)
+                                        .fill(Color(UIColor.systemGray6))
+                                    HStack {
+                                        VStack(alignment: .leading) {
+                                            HStack {
+                                                Text(getIcon(type: theAppliances[i].name, index: theAppliances[i].index))
+                                                    .font(.title2)
+                                                    .padding(.leading)
+                                                Text(getApplianceName(type: theAppliances[i].name, index: theAppliances[i].index))
+                                                    .font(.title2)
+                                                Spacer()
+                                            }
+                                            Text(getProgram(type: theAppliances[i].name, index: theAppliances[i].index))
+                                                .font(.subheadline)
                                                 .padding(.leading)
-                                            Text(getApplianceName(type: theAppliances[i].name, index: theAppliances[i].index))
-                                                .font(.title2)
-                                            Spacer()
                                         }
-                                        Text(getProgram(type: theAppliances[i].name, index: theAppliances[i].index))
-                                            .font(.subheadline)
-                                            .padding(.leading)
+                                        Text(getTimeRemaining(type: theAppliances[i].name, index: theAppliances[i].index))
+                                            .font(.title)
+                                            .padding()
                                     }
-                                    Text(getTimeRemaining(type: theAppliances[i].name, index: theAppliances[i].index))
-                                        .font(.title)
-                                        .padding()
                                 }
                             }
                         }
+                        
                     }.padding(.horizontal)
                 }
         }.onAppear(perform: {let _ = self.updateTimer; fetchAppliances()})
